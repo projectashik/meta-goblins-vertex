9ab6bdd144571c0d84196ec8a5c6f5bbd926a096
Ashim <projectashik@gmail.com>
2023-01-21 22:52:40 +0545

    done faq data

--- src/components/sections/Faq.js
+++ src/components/sections/Faq.js
@@ -1,3 +1,4 @@
+import { faqData } from "@/data/faq"
 import { Accordion, Paper } from "@mantine/core"
 
 const Faq = () => {
@@ -9,31 +10,14 @@ const Faq = () => {
         <div className='max-w-4xl mx-auto px-4 mt-4'>
           <Paper withBorder>
             <Accordion>
-              <Accordion.Item value='customization'>
-                <Accordion.Control className='!font-bold'>
-                  Customization
-                </Accordion.Control>
-                <Accordion.Panel>
-                  Colors, fonts, shadows and many other parts are customizable
-                  to fit your design needs
-                </Accordion.Panel>
-              </Accordion.Item>
-
-              <Accordion.Item value='flexibility'>
-                <Accordion.Control>Flexibility</Accordion.Control>
-                <Accordion.Panel>
-                  Configure components appearance and behavior with vast amount
-                  of settings or overwrite any part of component styles
-                </Accordion.Panel>
-              </Accordion.Item>
-
-              <Accordion.Item value='focus-ring'>
-                <Accordion.Control>No annoying focus ring</Accordion.Control>
-                <Accordion.Panel>
-                  With new :focus-visible pseudo-class focus ring appears only
-                  when user navigates with keyboard
-                </Accordion.Panel>
-              </Accordion.Item>
+              {faqData.map((data, index) => (
+                <Accordion.Item value={`faq-${index}`}>
+                  <Accordion.Control className='!font-bold'>
+                    {data.question}
+                  </Accordion.Control>
+                  <Accordion.Panel>{data.answer}</Accordion.Panel>
+                </Accordion.Item>
+              ))}
             </Accordion>
           </Paper>
         </div>
---
+++ src/data/faq.js
@@ -0,0 +1,22 @@
+export const faqData = [
+  {
+    question: "What is LeftOverLift?",
+    answer:
+      "LeftOverLift is a platform that allows you to donate your leftover food to those in need. We are a non-profit organization that aims to reduce food waste and help those in need.",
+  },
+  {
+    question: "How does it work?",
+    answer:
+      "LeftOverLift is a platform that allows you to donate your leftover food to those in need. We are a non-profit organization that aims to reduce food waste and help those in need.",
+  },
+  {
+    question: "How do I donate?",
+    answer:
+      "LeftOverLift is a platform that allows you to donate your leftover food to those in need. We are a non-profit organization that aims to reduce food waste and help those in need.",
+  },
+  {
+    question: "How do I receive food?",
+    answer:
+      "LeftOverLift is a platform that allows you to donate your leftover food to those in need. We are a non-profit organization that aims to reduce food waste and help those in need.",
+  },
+]
