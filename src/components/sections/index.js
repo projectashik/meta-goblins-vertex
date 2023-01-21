4820794cccad7820f81ad563477d5001fafddc51
Ashim <projectashik@gmail.com>
2023-01-21 22:46:29 +0545

    fixed

--- src/components/sections/index.js
+++ src/components/sections/index.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-export { default as AboutSection } from "./About";
-export { default as Header } from "./Header";
-export { default as ServicesSection } from "./Services";
-export { default as Contributor } from "./Contributor";
-=======
 export { default as AboutSection } from "./About"
+export { default as Contributor } from "./Contributor"
 export { default as FaqSection } from "./Faq"
 export { default as Header } from "./Header"
 export { default as ServicesSection } from "./Services"
->>>>>>> b9d9880c4976a60caed9c14b144cc4c72eef9482
