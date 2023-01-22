9034f4f9cafb636c2d351eb4d9e19eb657251cd4
Roshan KC <roshankcpkr123@gmail.com>
2023-01-22 11:43:54 +0545

    fixed navbar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -1,41 +1,128 @@
-import { Button } from "@mantine/core"
-import Link from "next/link"
+import Link from "next/link";
+import { HiOutlineMenuAlt2 } from "react-icons/hi";
+import { ActionIcon } from "@mantine/core";
+import { useState } from "react";
+
+import { Menu, Button, Text } from "@mantine/core";
+// import {
+//   IconSettings,
+//   IconSearch,
+//   IconPhoto,
+//   IconMessageCircle,
+//   IconTrash,
+//   IconArrowsLeftRight,
+// } from "@tabler/icons";
 
 const Navbar = ({ setOpen }) => {
+  const [isNavOpen, setNavOpen] = useState(false);
+  const toggleNavbar = () => setNavOpen(!isNavOpen);
   return (
-    <div className='container h-16 flex items-center justify-between'>
-      <Link href='/' className='text-3xl font-bold'>
+    <div className="container pt-[20px] flex items-center justify-between">
+      <Link href="/" className="text-3xl font-bold">
         LeftOverLift
       </Link>
       <nav>
-        <ul className='flex items-center gap-4'>
+        <ul className="flex items-center gap-4 hidden md:flex ">
           <li>
-            <a href='#about' className='text-gray-700 font-bold'>
+            <a href="#about" className="text-gray-700 font-bold">
               About Us
             </a>
           </li>
           <li>
-            <a href='#about' className='text-gray-700 font-bold'>
+            <a href="#about" className="text-gray-700 font-bold">
               Services
             </a>
           </li>
           <li>
-            <a href='#about' className='text-gray-700 font-bold'>
+            <a href="#about" className="text-gray-700 font-bold">
               Contact
             </a>
           </li>
           <li>
             <Button
               onClick={() => setOpen(true)}
-              color='dark'
-              className='!text-primary'
+              color="dark"
+              className="!text-primary"
             >
               Sign Up
             </Button>
           </li>
         </ul>
+
+        <div className="px-5">
+          {" "}
+          <Menu shadow="md" width={"100%"} className="md:hidden">
+            <Menu.Target>
+              <ActionIcon onClick={toggleNavbar}>
+                <div className="md:hidden">
+                  <HiOutlineMenuAlt2 />
+                </div>
+              </ActionIcon>
+            </Menu.Target>
+
+            <Menu.Dropdown>
+              <Menu.Label>
+                <a href="#about" className="text-gray-700 font-bold">
+                  About Us
+                </a>
+              </Menu.Label>
+              <Menu.Label>
+                <a href="#about" className="text-gray-700 font-bold">
+                  Services
+                </a>
+              </Menu.Label>
+              <Menu.Label>
+                <a href="#about" className="text-gray-700 font-bold">
+                  Contact
+                </a>
+              </Menu.Label>
+              <Menu.Label>
+                <Button
+                  onClick={() => setOpen(true)}
+                  color="dark"
+                  className="!text-primary"
+                >
+                  Sign Up
+                </Button>
+              </Menu.Label>
+            </Menu.Dropdown>
+          </Menu>
+        </div>
       </nav>
+      {/* <nav>
+        <ul className="flex items-center gap-4 hidden md:flex ">
+          <li>
+            <a href="#about" className="text-gray-700 font-bold">
+              About Us
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="text-gray-700 font-bold">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="text-gray-700 font-bold">
+              Contact
+            </a>
+          </li>
+          <li>
+            <Button
+              onClick={() => setOpen(true)}
+              color="dark"
+              className="!text-primary"
+            >
+              Sign Up
+            </Button>
+          </li>
+        </ul>
+        <ActionIcon onClick={toggleNavbar}>
+          <div className="md:hidden">
+            <HiOutlineMenuAlt2 />
+          </div>
+        </ActionIcon>
+      </nav> */}
     </div>
-  )
-}
-export default Navbar
+  );
+};
+export default Navbar;
--- src/components/sections/Index/Header.js
+++ src/components/sections/Index/Header.js
@@ -1,41 +1,41 @@
-import { landingImages } from "@/data/landing-images"
-import { Button, Group } from "@mantine/core"
-import Image from "next/image"
-import { Navbar } from "../../layouts"
+import { landingImages } from "@/data/landing-images";
+import { Button, Group } from "@mantine/core";
+import Image from "next/image";
+import { Navbar } from "../../layouts";
 
 const Header = ({ setOpen }) => {
   return (
-    <header className='min-h-screen bg-primary'>
+    <header className="min-h-screen bg-primary">
       <Navbar setOpen={setOpen} />
 
-      <div className='container'>
-        <div className='grid grid-cols-2 items-center gap-9 lg:my-16'>
-          <div className='space-y-6'>
-            <h1 className='text-6xl font-bold leading-[61px]'>
+      <div className="py-8 container ">
+        <div className="grid md:grid-cols-2 grid-cols-1 items-center md:gap-9 lg:my-16">
+          <div className="md:space-y-6 space-y-2 order-last md:order-first">
+            <h1 className="md:text-6xl font-bold md:leading-[61px]">
               Revolutionalizing the way we manage food waste.
             </h1>
-            <p className='text-xl '>
+            <p className="text-xl ">
               Everything you need to connect your apps or games to decentralized
               networks. Powerful tools that simplify web3 development.
             </p>
 
             <Group>
-              <Button onClick={() => setOpen(true)} color='dark' size='lg'>
+              <Button onClick={() => setOpen(true)} color="dark" size="lg">
                 Sign Up
               </Button>
-              <Button variant='outline' size='lg' color='dark'>
+              <Button variant="outline" size="lg" color="dark">
                 Know More
               </Button>
             </Group>
           </div>
 
-          <div className='grid grid-cols-2 justify-items-center gap-2 '>
+          <div className="grid grid-cols-2 mb-8 md:mb-0 justify-items-center gap-2 ">
             {landingImages.map((image, index) => (
               <Image
                 src={image}
                 key={index}
-                className='aspect-square'
-                alt='LeftOverLift'
+                className="aspect-square"
+                alt="LeftOverLift"
                 width={"500"}
                 height={"500"}
               />
@@ -44,6 +44,6 @@ const Header = ({ setOpen }) => {
         </div>
       </div>
     </header>
-  )
-}
-export default Header
+  );
+};
+export default Header;
